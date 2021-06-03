1cb0061c832fff1cc932c73acaad057e67786d64
Mike Jones <jonesmg@beast.as.arizona.edu>
2021-06-03 10:50:26 -0700

    Added channel averaging and fields as arguments to baseline plotting tool.

--- baseline_plots.py
+++ baseline_plots.py
@@ -2,7 +2,7 @@ import sys
 
 vis = sys.argv[3]
 fields = sys.argv[4]
-ampmax = sys.argv[5]
+chanavg = sys.argv[5]
 
-command = "plotms(vis='{0}', gridrows=2, gridcols=2, xaxis='time', yaxis='amp',  field='{1}', iteraxis='baseline', exprange='all', xselfscale=True, yselfscale=True, plotrange=[0.,{2},0.,0.], plotfile='plots/baseline_plots/baseline_plot.png', showgui=False)".format(vis,fields,ampmax)
+command = "plotms(vis='{0}', gridrows=2, gridcols=2, xaxis='time', yaxis='amp', field='{1}', avgchannel='{2}', iteraxis='baseline', exprange='all', xselfscale=True, yselfscale=True, plotfile='plots/baseline_plots/baseline_plot.png', coloraxis='scan', showgui=False)".format(vis,fields,chanavg)
 exec(command)
