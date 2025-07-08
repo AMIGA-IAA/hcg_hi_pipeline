a79f2c52e6891608b6f34731a774e53985ce1d04
jonesmg <mike.jones.astro@gmail.com>
2025-07-08 16:01:19 -0700

    Added max amplitude as input.

--- baseline_plots.py
+++ baseline_plots.py
@@ -3,6 +3,7 @@ import sys
 vis = sys.argv[3]
 fields = sys.argv[4]
 chanavg = sys.argv[5]
+ampmax = sys.argv[6]
 
-command = "plotms(vis='{0}', gridrows=2, gridcols=2, xaxis='time', yaxis='amp', field='{1}', avgchannel='{2}', iteraxis='baseline', exprange='all', xselfscale=True, yselfscale=True, plotfile='plots/baseline_plots/baseline_plot.png', coloraxis='scan', showgui=False)".format(vis,fields,chanavg)
+command = "plotms(vis='{0}', gridrows=2, gridcols=2, xaxis='time', yaxis='amp', field='{1}', spw='0', avgchannel='{2}', iteraxis='baseline', exprange='all', xselfscale=True, yselfscale=True, plotrange=[0,0,0,{3}], plotfile='plots/baseline_plots/baseline_plot.png', coloraxis='scan', showgui=False)".format(vis,fields,chanavg,ampmax)
 exec(command)
